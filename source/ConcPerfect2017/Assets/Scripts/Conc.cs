301d160af29735fdb55f9b86319d421bbf57561f
David Bohan <madvlad00@googlemail.com>
2016-12-31 17:51:47 -0800

    Add script files from POC demo

---
+++ source/ConcPerfect2017/Assets/Scripts/Conc.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Conc : MonoBehaviour
+{
+    public AudioClip timerSFX;
+    public AudioClip explodeSFX;
+    public float timer = 4.0f;
+    public bool exploded = false;
+    void Start()
+    {
+        Invoke("Explode", timer);
+        if (timerSFX)
+        {
+            if (gameObject.GetComponent<AudioSource>())
+            {
+                gameObject.GetComponent<AudioSource>().PlayOneShot(timerSFX);
+            }
+            else
+            {
+                AudioSource.PlayClipAtPoint(timerSFX, gameObject.transform.position);
+            }
+        }
+    }
+
+    void Explode()
+    {
+        exploded = true;
+        Invoke("Destroy", timer);
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+        EnactPush();
+        PlayExplosionSound();
+    }
+
+    void EnactPush()
+    {
+        var colliders = Physics.OverlapSphere(transform.position, 10.0f);
+        foreach (var hit in colliders)
+        {
+            if (hit.CompareTag("Player"))
+            {
+                var receiver = hit.GetComponent<ImpactReceiver>();
+                if (receiver)
+                {
+                    var dir = hit.transform.position - transform.position;
+                    float force;
+                    if (dir.magnitude == 0)
+                    {
+                        force = Mathf.Clamp(100.0f, 0, 100.0f);
+                        dir = hit.GetComponent<CharacterController>().velocity * 10;
+                    }
+                    else
+                    {
+                        force = Mathf.Clamp(100.0f, 0, 100.0f) * dir.magnitude;
+                    }
+                    Debug.Log(dir);
+                    receiver.AddImpact(dir, force);
+                }
+            }
+        }
+    }
+
+    void PlayExplosionSound()
+    {
+        if (explodeSFX)
+        {
+            if (gameObject.GetComponent<AudioSource>())
+            {
+                gameObject.GetComponent<AudioSource>().PlayOneShot(explodeSFX);
+            }
+            else
+            {
+                AudioSource.PlayClipAtPoint(explodeSFX, gameObject.transform.position);
+            }
+        }
+    }
+
+    void Destroy()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ source/ConcPerfect2017/Assets/Scripts/Concer.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Concer : MonoBehaviour
+{
+
+    public GameObject concPrefab;
+    private bool primed = false;
+    private float timer = 0.0f;
+    private GameObject concInstance;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        if (Input.GetButtonDown("Conc"))
+        {
+            if (timer <= 0)
+            {
+                timer = 0.45f;
+                primed = true;
+                concInstance = Instantiate(concPrefab, transform.position, transform.rotation) as GameObject;
+                if (!concInstance.GetComponent<Rigidbody>()) { concInstance.AddComponent<Rigidbody>(); }
+                concInstance.GetComponent<Rigidbody>().useGravity = false;
+                concInstance.GetComponent<BoxCollider>().enabled = false;
+                concInstance.GetComponent<MeshRenderer>().enabled = false;
+            }
+        }
+        if (primed && !Input.GetButton("Conc"))
+        {
+            if (concPrefab && !concInstance.GetComponent<Conc>().exploded)
+            {
+                concInstance.transform.position = transform.position + transform.forward;
+                concInstance.GetComponent<BoxCollider>().enabled = true;
+                concInstance.GetComponent<MeshRenderer>().enabled = true;
+                concInstance.GetComponent<Rigidbody>().useGravity = true;
+                concInstance.GetComponent<Rigidbody>().AddForce(transform.forward * 8, ForceMode.Impulse);
+                primed = false;
+            }
+        }
+        else if (primed)
+        {
+            concInstance.transform.position = transform.position;
+        }
+    }
+}
