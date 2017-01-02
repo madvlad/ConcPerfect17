22be51ec93066edd6c913889e9d9b36688cba30c
David Bohan <madvlad00@googlemail.com>
2017-01-02 15:29:48 -0500

    Add ammo count to concer script

---
+++ source/ConcPerfect2017/Assets/Scripts/ConcPack.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConcPack : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        var currentConcCount = other.GetComponent<Concer>().ConcCount;
+        if (currentConcCount < 3)
+        {
+            var concsToAdd = 3 - other.GetComponent<Concer>().ConcCount;
+            other.GetComponent<Concer>().ConcCount += concsToAdd;
+            Destroy(this.gameObject);
+        }
+    }
+}
