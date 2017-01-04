7787ea061b918092920e11001144bc9412142aac
David Bohan <madvlad00@googlemail.com>
2017-01-03 21:04:15 -0500

    Add iterative level generation capability
    This is for pre-built level sequences that will be defined by the user
    and not rely on a seed. Also included is a start and end prefab for
    capping off courses

--- source/ConcPerfect2017/Assets/Scripts/LevelGenerator.cs
+++ source/ConcPerfect2017/Assets/Scripts/LevelGenerator.cs
@@ -3,32 +3,64 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour {
+    public GameObject startPrefab;
+    public GameObject endPrefab;
     public List<GameObject> jumpList;
+    private List<GameObject> courseJumpList;
     public int jumpNumber;
     public int RandomSeed;
 
-	void Start () {
+	void Start ()
+    {
         if (RandomSeed != 0)
         {
             Random.InitState(RandomSeed);
         }
 
-        GameObject previousSnapPoint = null;
+        // TODO :: Display this to the player
+        Debug.Log("Seed being used: " + Random.seed);
+
+        GameObject previousSnapPoint = InstantiateStartPoint();
 
         for (int i = 0; i < jumpNumber; i++)
         {
             var nextJump = Random.Range(0, jumpList.Count);
-            var newJump = Instantiate(jumpList[nextJump]);
-
-            if (previousSnapPoint != null)
-            {
-                newJump.GetComponent<SnapPointManager>().snapPointIn.transform.position = previousSnapPoint.transform.position;
-            }
-            previousSnapPoint = newJump.GetComponent<SnapPointManager>().snapPointOut;
+            previousSnapPoint = InstantiateJumpAtSnapPoint(previousSnapPoint, jumpList[nextJump]);
         }
-	}
-	
-	void Update () {
-		
-	}
+
+        InstantiateEndPoint(previousSnapPoint);
+    }
+
+    void BuildCourseIteratively()
+    {
+        GameObject previousSnapPoint = InstantiateStartPoint();
+
+        foreach (var jump in courseJumpList)
+        {
+            previousSnapPoint = InstantiateJumpAtSnapPoint(previousSnapPoint, jump);
+        }
+
+        InstantiateEndPoint(previousSnapPoint);
+    }
+
+    private static GameObject InstantiateJumpAtSnapPoint(GameObject previousSnapPoint, GameObject jump)
+    {
+        var newJump = Instantiate(jump);
+        newJump.GetComponent<SnapPointManager>().snapPointIn.transform.position = previousSnapPoint.transform.position;
+        previousSnapPoint = newJump.GetComponent<SnapPointManager>().snapPointOut;
+        return previousSnapPoint;
+    }
+
+    private GameObject InstantiateStartPoint()
+    {
+        var newStartPrefab = Instantiate(startPrefab);
+        GameObject previousSnapPoint = newStartPrefab.GetComponent<SnapPointManager>().snapPointOut;
+        return previousSnapPoint;
+    }
+
+    private void InstantiateEndPoint(GameObject previousSnapPoint)
+    {
+        var newEndPrefab = Instantiate(endPrefab);
+        newEndPrefab.GetComponent<SnapPointManager>().snapPointIn.transform.position = previousSnapPoint.transform.position;
+    }
 }
