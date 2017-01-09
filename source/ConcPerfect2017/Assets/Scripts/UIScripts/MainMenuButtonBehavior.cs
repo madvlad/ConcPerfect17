e64667ab496471664fd4a481a8b2b58f6dde217b
David Bohan <madvlad00@googlemail.com>
2017-01-08 21:52:45 -0500

    Add splash screen and main menu

---
+++ source/ConcPerfect2017/Assets/Scripts/UIScripts/MainMenuButtonBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtonBehavior : MonoBehaviour {
+    public string sceneToLoad;
+
+    void StartNewGame()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+        SceneManager.UnloadSceneAsync("MainMenuScene");
+    }
+
+    void QuitApplication()
+    {
+        Application.Quit();
+    }
+}
