901e438d6d33a9bfd7f5c9f48b7679be28b99938
David Bohan <madvlad00@googlemail.com>
2017-01-15 00:01:23 -0500

    Add movable platforms

---
+++ source/ConcPerfect2017/Assets/Scripts/MovablePlatform.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovablePlatform : MonoBehaviour {
+
+    public List<GameObject> WayPoints;
+    public float speed;
+
+    private GameObject nextWayPoint;
+    private int idx = 0;
+
+    void Start()
+    {
+
+    }
+
+	void Update () {
+        nextWayPoint = WayPoints[idx];
+        if (Vector3.Distance(gameObject.transform.position, nextWayPoint.transform.position) < 0.5)
+        {
+            var a = new WaitForSecondsRealtime(4.0f);
+            if (idx == WayPoints.Count - 1)
+            {
+                idx = 0;
+            }
+            else
+            {
+                idx++;
+            }
+
+            nextWayPoint = WayPoints[idx];
+        }
+
+        gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, nextWayPoint.transform.position, speed);
+	}
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.parent = gameObject.transform;
+        }
+    }
+
+    void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.parent = null;
+        }
+    }
+}
