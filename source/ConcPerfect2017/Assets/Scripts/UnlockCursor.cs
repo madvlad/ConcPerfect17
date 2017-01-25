3f7f7180efe0c51d09bf210cc57b79e930adb983
David Bohan <madvlad00@googlemail.com>
2017-01-24 21:47:21 -0500

    Unlock cursor on main menu

---
+++ source/ConcPerfect2017/Assets/Scripts/UnlockCursor.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockCursor : MonoBehaviour {
+	void Start () {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+	}
+}
