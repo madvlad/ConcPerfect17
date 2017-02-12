4756599e03d8d108b27c5f33467f9db1efe4cd5d
David Bohan <madvlad00@googlemail.com>
2017-02-12 11:48:23 -0500

    Add course history manager
    Issues addressed: #63, #64, #65, #66

---
+++ source/ConcPerfect2017/Assets/Scripts/CourseHistoryManager.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CourseHistoryManager : MonoBehaviour {
+
+    private CourseHistoryEntry previousBestTime;
+    
+    public void StoreNewRecord(string CourseSeed, float TimeCompleted, bool IsFavorited)
+    {
+        var entry = new CourseHistoryEntry { CourseSeed = CourseSeed, TimeCompleted = TimeCompleted, IsFavorited = IsFavorited };
+        var currentRecords = GetCurrentRecords();
+        bool listChanged = false;
+
+        if (IsFirstRecord(currentRecords, entry))
+        {
+            listChanged = true;
+            currentRecords.Add(entry);
+        }
+        else if (IsBestTime(currentRecords, entry))
+        {
+            listChanged = true;
+            currentRecords.Remove(previousBestTime);
+            currentRecords.Add(entry);
+        }
+
+        if (listChanged)
+        {
+            SerializeAndSave(currentRecords);
+        }
+    }
+
+    private bool IsFirstRecord(List<CourseHistoryEntry> currentRecords, CourseHistoryEntry entry)
+    {
+        foreach (var record in currentRecords)
+        {
+            if (String.Equals(record.CourseSeed, entry.CourseSeed))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private bool IsBestTime(List<CourseHistoryEntry> currentRecords, CourseHistoryEntry entry)
+    {
+        foreach (var record in currentRecords)
+        {
+            if (String.Equals(record.CourseSeed, entry.CourseSeed) && entry.TimeCompleted < record.TimeCompleted)
+            {
+                previousBestTime = record;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private List<CourseHistoryEntry> GetCurrentRecords()
+    {
+        if (PlayerPrefs.HasKey("CourseRecords"))
+        {
+            var serializedList = PlayerPrefs.GetString("CourseRecords");
+            return JsonUtility.FromJson<List<CourseHistoryEntry>>(serializedList);
+        }
+        else
+        {
+            return new List<CourseHistoryEntry>();
+        }
+    }
+
+    private void SerializeAndSave(List<CourseHistoryEntry> currentRecords)
+    {
+        string serializedList = JsonUtility.ToJson(currentRecords);
+        PlayerPrefs.SetString("CourseRecords", serializedList);
+    }
+
+    void Start () { }
+	void Update () { }
+
+    [Serializable]
+    private class CourseHistoryEntry
+    {
+        public string CourseSeed;
+        public float TimeCompleted;
+        public bool IsFavorited;
+    }
+}
