c2d13fa3d6022acca27e4852143b285b829dca8d
David Bohan <madvlad00@googlemail.com>
2017-02-19 09:24:43 -0500

    Tie version number display to application manager'

---
+++ source/ConcPerfect2017/Assets/Scripts/UIScripts/VersionInformation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VersionInformation : MonoBehaviour {
+
+    public Text textUI;
+
+	void Start () {
+        textUI.text = ApplicationManager.APPLICATION_VERSION;	
+	}
+}
