79d733d90a41fff0fc6aa1acd70e3a9044b33cd4
David Bohan <madvlad00@googlemail.com>
2017-03-19 15:23:53 -0400

    Add skin select to settings menu
    Issues addressed: #177

---
+++ source/ConcPerfect2017/Assets/Scripts/UIScripts/PlayerSkinSelectBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSkinSelectBehavior : MonoBehaviour {
+
+    public List<Material> playerSkins;
+    public GameObject playerModel;
+
+    private int currentSkinIndex;
+	
+	void Start () {
+		
+	}
+	
+    void SetPlayerSkin(Material newSkin)
+    {
+        playerModel.GetComponent<SkinnedMeshRenderer>().material = newSkin;
+    }
+
+    public void NextSkin()
+    {
+        currentSkinIndex++;
+
+        if (currentSkinIndex >= playerSkins.Count)
+        {
+            currentSkinIndex = 0;
+            SetPlayerSkin(playerSkins[0]);
+        }
+
+        SetPlayerSkin(playerSkins[currentSkinIndex]);
+    }
+
+    public void PreviousSkin()
+    {
+        currentSkinIndex--;
+
+        if (currentSkinIndex < 0)
+        {
+            currentSkinIndex = playerSkins.Count - 1;
+        }
+
+        SetPlayerSkin(playerSkins[currentSkinIndex]);
+    }
+}
