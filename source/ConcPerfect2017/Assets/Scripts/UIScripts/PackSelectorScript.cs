ead17c6a22508ebfbbfbdc51b00a41c0890491b6
David Bohan <madvlad00@googlemail.com>
2017-06-28 22:58:56 -0400

    Add pages to premade levels

---
+++ source/ConcPerfect2017/Assets/Scripts/UIScripts/PackSelectorScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PackSelectorScript : MonoBehaviour {
+	public List<GameObject> levelPacks;
+
+	private int index = 0;
+
+	// Use this for initialization
+	void Start () {
+		index = 0;
+		levelPacks[0].SetActive(true);
+	}
+
+	public void ForwardOnePage()
+	{
+		levelPacks[index].SetActive(false);
+
+		index++;
+
+		if (index == levelPacks.Count)
+			index = 0;
+
+		levelPacks[index].SetActive(true);
+	}
+
+	public void BackOnePage()
+	{
+		levelPacks[index].SetActive(false);
+
+		index--;
+
+		if (index < 0)
+			index = levelPacks.Count - 1;
+
+		levelPacks[index].SetActive(true);
+	}
+}
