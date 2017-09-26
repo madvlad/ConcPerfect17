571812efd27819dbade140f66fe2f47a08d7c410
Wesley King <kingwe92@gmail.com>
2017-09-26 00:02:50 -0400

    Add menu item for Concmination map
    Enhancement #318

--- source/ConcPerfect2017/Assets/Scripts/LevelManager.cs
+++ source/ConcPerfect2017/Assets/Scripts/LevelManager.cs
@@ -12,6 +12,7 @@ public class LevelManager : MonoBehaviour {
 	public List<GameObject> level6;
     public List<GameObject> level7;
     public List<GameObject> level8;
+    public List<GameObject> level9;
 
     internal List<GameObject> getLevel(int currentLevel)
     {
@@ -33,6 +34,8 @@ public class LevelManager : MonoBehaviour {
                 return level7;
             case 8:
                 return level8;
+            case 9:
+                return level9;
             default:
                 return null;
         }
