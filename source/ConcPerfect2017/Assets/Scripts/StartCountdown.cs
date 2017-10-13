7573554f6f2cfc9d2abfe1913870b0b84c7601c3
Wesley King <kingwe92@gmail.com>
2017-10-12 22:21:59 -0400

    Add Concmination Match Start Behaviour
    - #323
    - OnTriggerEnter, start a timer of 15 seconds
    - When timer hits the end of 15 seconds, remove start barrier

---
+++ source/ConcPerfect2017/Assets/Scripts/StartCountdown.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartCountdown : MonoBehaviour {
+    public GameObject CountdownPanel;
+    float timer = 15;
+    bool started = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!started)
+            return;
+        timer -= Time.deltaTime;
+        CountdownPanel.GetComponent<Text>().text = Mathf.CeilToInt(timer).ToString();
+        if (timer < 0) {
+            StopCountdown();
+        }
+	}
+
+    public void StartCountDown() {
+        started = true;
+        timer = 15;
+        CountdownPanel.GetComponent<Text>().enabled = true;
+    }
+
+    void StopCountdown() {
+        started = false;
+        CountdownPanel.GetComponent<Text>().enabled = false;
+    }
+}
